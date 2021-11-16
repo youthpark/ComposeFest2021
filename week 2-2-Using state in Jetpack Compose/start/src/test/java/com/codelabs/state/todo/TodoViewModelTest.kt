1ad495d7e7664ba64f6ffead30898d074b52d717
youth.park <youthpark82@gmail.com>
2021-11-16 19:46:35 +0900

    week2 - <Using state in Jetpack Compose> step 10. Test State in ViewModel

--- week 2-2-Using state in Jetpack Compose/start/src/test/java/com/codelabs/state/todo/TodoViewModelTest.kt
+++ week 2-2-Using state in Jetpack Compose/start/src/test/java/com/codelabs/state/todo/TodoViewModelTest.kt
@@ -16,6 +16,93 @@
 
 package com.codelabs.state.todo
 
+import com.codelabs.state.util.generateRandomTodoItem
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
 class TodoViewModelTest {
-    // TODO: Write tests
+
+    @Test
+    fun whenAddItem_updatesList() {
+        val subject = TodoViewModel()
+        val expected = generateRandomTodoItem()
+        subject.addItem(expected)
+        assertThat(subject.todoItems).isEqualTo(listOf(expected))
+    }
+
+    @Test
+    fun whenRemovingItem_updatesList() {
+        val subject = TodoViewModel()
+        val item1 = generateRandomTodoItem()
+        val item2 = generateRandomTodoItem()
+        subject.addItem(item1)
+        subject.addItem(item2)
+        subject.removeItem(item1)
+        assertThat(subject.todoItems).isEqualTo(listOf(item2))
+    }
+
+    @Test
+    fun whenNotEditing_currentEditItemIsNull() {
+        val subject = TodoViewModel()
+        val item = generateRandomTodoItem()
+        subject.addItem(item)
+        assertThat(subject.currentEditItem).isNull()
+    }
+
+    @Test
+    fun whenEditing_currentEditItemIsCorrectItem() {
+        val subject = TodoViewModel()
+        val item1 = generateRandomTodoItem()
+        val item2 = generateRandomTodoItem()
+        subject.addItem(item1)
+        subject.addItem(item2)
+        subject.onEditItemSelected(item1)
+        assertThat(subject.currentEditItem).isEqualTo(item1)
+    }
+
+    @Test
+    fun whenEditingDone_currentEditItemIsCorrectItem() {
+        val subject = TodoViewModel()
+        val item1 = generateRandomTodoItem()
+        val item2 = generateRandomTodoItem()
+        subject.addItem(item1)
+        subject.addItem(item2)
+        subject.onEditItemSelected(item1)
+        subject.onEditDone()
+        assertThat(subject.currentEditItem).isNull()
+    }
+
+    @Test
+    fun whenEditingItem_updatesAreShownInItemAndList() {
+        val subject = TodoViewModel()
+        val item1 = generateRandomTodoItem()
+        val item2 = generateRandomTodoItem()
+        subject.addItem(item1)
+        subject.addItem(item2)
+        subject.onEditItemSelected(item1)
+        val expected = item1.copy(task = "Update for test case")
+        subject.onEditItemChange(expected)
+        assertThat(subject.todoItems).isEqualTo(listOf(expected, item2))
+        assertThat(subject.currentEditItem).isEqualTo(expected)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun whenEditing_wrongItemThrows() {
+        val subject = TodoViewModel()
+        val item1 = generateRandomTodoItem()
+        val item2 = generateRandomTodoItem()
+        subject.addItem(item1)
+        subject.addItem(item2)
+        subject.onEditItemSelected(item1)
+        val expected = item2.copy(task = "Update for test case")
+        subject.onEditItemChange(expected)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun whenNotEditing_onEditItemChangeThrows() {
+        val subject = TodoViewModel()
+        val item = generateRandomTodoItem()
+        subject.onEditItemChange(item)
+    }
+
 }
